f559d79055eeaae130cf98ad5afa7910d9e0ee64
Gustavo Frazão <gustavofrazao@outlook.com>
2023-08-21 11:21:08 -0300

    nova2

--- controle-candidato/src/ProcessoSeletivo.java
+++ controle-candidato/src/ProcessoSeletivo.java
@@ -1,9 +1,40 @@
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo {
     public static void main(String[] args){
         // System.out.println("Processo seletivo");
-        selecaoCandidatos();
+        //selecaoCandidatos();
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
+        for (String candidato: candidatos){
+            entrandoEmContato(candidato);
+
+        }
     }
+    static void entrandoEmContato(String candidato) {
+		
+		int tentativasRealizadas = 1;
+		boolean continuarTentando = true;
+		boolean atendeu=false;
+		do {
+			atendeu= atender();
+			continuarTentando = !atendeu;
+			if(continuarTentando)
+				tentativasRealizadas++;
+			else
+				System.out.println("CONTATO REALIZADO COM SUCESSO");
+			
+		}while(continuarTentando && tentativasRealizadas<3);
+		
+		if(atendeu)
+			System.out.println("CONSEGUIMOS CONTATO COM " + candidato +" NA " + tentativasRealizadas + " TENTATIVA");
+		else
+			System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato +", NÚMERO MAXIMO TENTATIVAS " + tentativasRealizadas + " REALIZADA");
+		
+		
+	}
+    static boolean atender() {
+		return new Random().nextInt(3)==1;	
+	}
     static void selecaoCandidatos(){
         String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
         int candidatosSelecionados = 0;
